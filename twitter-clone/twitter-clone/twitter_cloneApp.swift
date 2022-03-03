fd0f4ee69c0d36add09872f53a9ae5b1e0f49fb5
lawwayne <72894553+lawwayne@users.noreply.github.com>
2022-03-02 22:53:02 -0500

    initial push

---
+++ twitter-clone/twitter-clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter-clone
+//
+//  Created by Student on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
